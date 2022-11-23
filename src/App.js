21e3c44ff1d2f93fc0ff110cc0898d1b9010fdf6
bonghee <hae8064@naver.com>
2022-11-23 22:53:29 +0900

    api axios작업 중.. cors 이슈 발생

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'http://www.opinet.co.kr/api/areaAvgRecentPrice.do?out=json&code=${process.env.REACT_APP_GASAPI}&area=11'
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  // const getData = async () => {
+  //   const res = await fetch(
+  //     //서울 기준 json형식으로 데이터 받기 원한다.
+  //     `http://www.opinet.co.kr/api/areaAvgRecentPrice.do?out=json&code=${process.env.REACT_APP_GASAPI}&area=11`
+  //   ).then((res) => res.json());
+  // };
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">주유소 검색</div>
+      <button className="apiClick" onClick={onClick}>
+        api버튼
+      </button>
+      데이터 가져오기: {data}
+    </>
   );
 }
 
