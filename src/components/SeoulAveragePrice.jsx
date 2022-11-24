2059dff62191f294dedc3063fb444e4da36acdca
bonghee <hae8064@naver.com>
2022-11-24 17:06:41 +0900

    서울평균 주유소 가격 useEffect를 통해 처음부터 렌더링

--- src/components/SeoulAveragePrice.jsx
+++ src/components/SeoulAveragePrice.jsx
@@ -1,33 +1,61 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function SeoulAveragePrice() {
   const [data, setData] = useState([]);
 
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
+  // const response = async () => {
+  //   await axios.get(
+  //     `https://cors-anywhere.herokuapp.com/www.opinet.co.kr/api/avgSidoPrice.do?out=json&code=${process.env.REACT_APP_GASAPI}&sido=01`
+  //   );
+  //   setData((data) => {
+  //     return [...data, response.data.RESULT.OIL[0].PRICE];
+  //   });
+  //   setData((data) => {
+  //     return [...data, response.data.RESULT.OIL[3].PRICE];
+  //   });
+  //   console.log(response);
+  // };
+
+  useEffect(() => {
+    async function response() {
+      const result = await axios.get(
         `https://cors-anywhere.herokuapp.com/www.opinet.co.kr/api/avgSidoPrice.do?out=json&code=${process.env.REACT_APP_GASAPI}&sido=01`
       );
       setData((data) => {
-        return [...data, response.data.RESULT.OIL[0].PRICE];
+        return [...data, result.data.RESULT.OIL[0].PRICE];
       });
       setData((data) => {
-        return [...data, response.data.RESULT.OIL[3].PRICE];
+        return [...data, result.data.RESULT.OIL[3].PRICE];
       });
-      console.log(response);
-    } catch (e) {
-      console.log(e);
     }
-  };
+    response();
+  }, []);
+
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `https://cors-anywhere.herokuapp.com/www.opinet.co.kr/api/avgSidoPrice.do?out=json&code=${process.env.REACT_APP_GASAPI}&sido=01`
+  //     );
+  //     setData((data) => {
+  //       return [...data, response.data.RESULT.OIL[0].PRICE];
+  //     });
+  //     setData((data) => {
+  //       return [...data, response.data.RESULT.OIL[3].PRICE];
+  //     });
+  //     console.log(response);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
   return (
     <div>
-      <button className="apiClick" onClick={onClick}>
+      {/* <button className="apiClick" onClick={onClick}>
         서울 평균 기름값
-      </button>
+      </button> */}
       <br />
-      데이터 가져오기: 휘발유: {data[0]}, 경유: {data[1]}
+      휘발유: {data[0]}, 경유: {data[1]}
     </div>
   );
 }
