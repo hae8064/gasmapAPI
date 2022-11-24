7d9d176e477ef345a819f18a89ad8d665315f265
bonghee <hae8064@naver.com>
2022-11-24 15:39:09 +0900

    네이버 맵 api 사용 & geolocation 사용해 내 위도 경도 가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
 import SeoulAveragePrice from "./components/SeoulAveragePrice.jsx";
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from "react-naver-maps";
+import NaverMapConatiner from "./components/NaverMapConatiner";
+import axios from "axios";
+import geolocation from "geolocation";
+import useGeolocation from "./hooks/useGeolocation.tsx";
 
 function App() {
-  //D047 경유
+  // 컴포넌트 안쪽에서 선언하면 에러 발생
+  const location = useGeolocation();
+  const [lat, setLat] = useState();
+  const [lng, setLng] = useState();
+
+  useEffect(() => {
+    setLat(location.coordinates.lat);
+    setLng(location.coordinates.lng);
+  }, [location]);
+
   return (
     <>
-      <div className="App">주유소 검색</div>
+      <div className="mainTitle">Gas Station</div>
+      <RenderAfterNavermapsLoaded
+        ncpClientId={process.env.REACT_APP_NAVERMAP}
+        error={<p>Maps Load Error</p>}
+        // loading={<p>Maps Loading...</p>}
+        loading={<NaverMapConatiner />}
+      ></RenderAfterNavermapsLoaded>
+      {/* 내위치 확인 위도 경도  */}
+      위도:{lat}
+      경도: {lng}
+      {/* 서울시 평균 가격 */}
       <SeoulAveragePrice />
     </>
   );
---
+++ src/components/NaverMapConatiner.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from "react-naver-maps";
+
+const NaverMapConatiner = () => {
+  return (
+    <div>
+      <NaverMap
+        mapDivId={"maps-getting-started-uncontrolled"} // default: react-naver-map
+        style={{
+          width: "100%", // 네이버지도 가로 길이
+          height: "30vh", // 네이버지도 세로 길이
+        }}
+        defaultCenter={{ lat: 37.5408428, lng: 126.9458903 }} // 지도 초기 위치
+        defaultZoom={16} // 지도 초기 확대 배율
+        zoomControl={true}
+      >
+        <Marker
+          key={1}
+          position={{ lat: 37.5408428, lng: 126.9458903 }}
+          animation={2}
+          clickable={true}
+          onClick={() => {
+            alert("현재 위치입니다.");
+          }}
+        />
+      </NaverMap>
+    </div>
+  );
+};
+
+export default NaverMapConatiner;
